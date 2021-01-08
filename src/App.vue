664cfe4694f4a2f6632bd01d0380a814668a16a4
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-07 19:17:23 -0600

    made methods, computed, and mounted

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers: </strong> {{ followers }}
+    <button @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_mdreesen',
+        firstName: 'Michael',
+        lastName: 'Dreesen',
+        email: 'mdreesen90@gmail.com',
+        isAdmon: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+      // this.followers = this.followers + 1
+      // this.followers += 1
+    }
+  },
+  mounted() {
+    // running the followUser function
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
